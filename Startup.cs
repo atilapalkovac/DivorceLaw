926b10213c4658c96b886440898f5a7f678541c6
atilapalkovac <39304882+atilapalkovac@users.noreply.github.com>
2020-04-27 10:37:30 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DivorceLaw.Startup))]
+namespace DivorceLaw
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
